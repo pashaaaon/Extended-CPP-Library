47248679048d9f58508753874c95d8ac6b9ee836
pasha-oni <legendarypasha64@gmail.com>
2026-07-06 23:09:57 +0300

    Add split_string() ; remove_prefix(); remove_suffix()

--- extd_library.hpp
+++ extd_library.hpp
@@ -12,29 +12,88 @@ namespace extd {
 
     // String Methods
 
-    std::vector<std::string> split_string(std::string target, std::string splitter, int count = false, bool StartFromEnd = false) {
+    std::vector<std::string> split_string(std::string target_string, std::string splitter, int count = false, bool StartFromEnd = false) {
         using namespace std;
 
         vector<string> Result;
         string Current = "";
-        int size_of_splitter = splitter.size();
-        int size_of_current_read = 0;
+        int current_count = 0;
+
+        if (count == false) {count = target_string.size();}
 
         if (!StartFromEnd) {
-            for (char i : target) {
+            for (int i = 0; i<target_string.size(); i+=1) {
                 bool check = false;
-                if (i == splitter[0]) {
-                    for (int j = 1; j<size_of_splitter; j+=1) {
-                        if (splitter[j] != target[i+j]) {continue;}
+                if (current_count == count) {
+                    for (int j = i; j<target_string.size(); j+=1) {
+                        Current += target_string[j];
                     }
-                    check = true;
+                    break;
                 }
-                if (check) {Result.push_back(Current);}
-                else {Current += i;}
+                if (target_string[i] == splitter[0]) {
+                    check = true;
+                    for (int j = 0; j<splitter.size(); j+=1) {
+                        if (i+j >= target_string.size()) {check = false; break;}
+                        if (splitter[j] != target_string[i+j]) {check = false; break;}
+                    }
+                }
+                if (check) {Result.push_back(Current); Current = ""; i+=splitter.size()-1; current_count+=1;}
+                else {Current += target_string[i];}
             }
+            if (Current != "") {Result.push_back(Current);}
         }
         else {
-
+            for (int i = target_string.size()-1; i>-1; i-=1) {
+                bool check = false;
+                if (current_count == count) {
+                    for (int j = i; j>-1; j-=1) {
+                        Current = string(1, target_string[j]) + Current;
+                    }
+                    break;
+                }
+                if (target_string[i] == splitter[0]) {
+                    check = true;
+                    for (int j = 0; j<splitter.size(); j+=1) {
+                        if (i-j <= -1) {check = false; break;}
+                        if (splitter[j] != target_string[i-j]) {check = false; break;}
+                    }
+                }
+                if (check) {Result.insert(Result.begin(), Current); Current = ""; i-=splitter.size()-1; current_count+=1;}
+                else {Current = string(1, target_string[i]) + Current;}
+            }
+            if (Current != "") {Result.insert(Result.begin(), Current);}
         }
+        
+        return Result;
     }
+
+    std::string remove_prefix(std::string target_string, std::string prefix) {
+        if (prefix.size() > target_string.size()) {return target_string;}
+
+        for (int i = 0; i<prefix.size(); i+=1) {
+            if (prefix[i] != target_string[i]) {return target_string;}
+        }
+
+        std::string Result = "";
+        for (int i = prefix.size(); i<target_string.size(); i+=1) {
+            Result += target_string[i];
+        }
+
+        return Result;
+    };
+
+    std::string remove_suffix(std::string target_string, std::string suffix) {
+        if (suffix.size() > target_string.size()) {return target_string;}
+
+        for (int i = 0; i<suffix.size(); i+=1) {
+            if (suffix[i] != target_string[i + target_string.size()-suffix.size()]) {return target_string;}
+        }
+
+        std::string Result = "";
+        for (int i = 0; i<target_string.size()-suffix.size(); i+=1) {
+            Result += target_string[i];
+        }
+
+        return Result;
+    };
 }
---
+++ main.cpp
@@ -0,0 +1,11 @@
+#include "extd_library.hpp"
+
+using namespace std;
+using namespace extd;
+
+int main() {
+    vector<string> arr1 = {"4331", "doahoihd", "heks"};
+    string test_str1 = "abc1312";
+    cout << remove_suffix(test_str1, "abc");
+    return 0;
+}
